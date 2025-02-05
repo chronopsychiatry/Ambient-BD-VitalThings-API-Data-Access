47de6b85507b69fb7200cb6278277824b3f432b2
DanielThedie <dthedie@gmail.com>
2025-02-05 14:41:44 +0000

    Updated tests

--- tests/test_data_download.py
+++ tests/test_data_download.py
@@ -1,165 +1,148 @@
 import json
 import os
 import tempfile
-import unittest
 import datetime
+import pytest
 
-from download.data_download import DataDownloader
-from sf_api.dom import Session, User
-from sf_api.somnofy import Somnofy, SESSION_DATA_OUTPUTS
-from storage.paths_resolver import PathsResolver
+from ambient_bd_downloader.download.data_download import DataDownloader
+from ambient_bd_downloader.sf_api.dom import Session, Subject
+from ambient_bd_downloader.sf_api.somnofy import Somnofy
+from ambient_bd_downloader.storage.paths_resolver import PathsResolver
 
 
 # make class TestSomnofy subclass of Somnofy
 class MockSomnofy(Somnofy):
 
-    # define method __init__
     def __init__(self):
-        self.test_session_json = os.path.join('data','2024-07-20_VFNPVRgHFAA1AwAA_raw.json')
+        self.test_session_json = './tests/data/2024-12-25_RkdSRhgMGQ8XFQAA_raw.json'
         print(f'mock session_json: {os.path.abspath(self.test_session_json)}')
 
     def _read_test_session_json(self):
         with open(self.test_session_json, 'r') as f:
             return json.load(f)
 
-    # define method get_users
-    def get_users(self):
+    def get_subjects(self):
         # return list of dictionaries
-        return [{'id': '1', 'last_name': 'user1'}, {'id': '2', 'last_name': 'user2'},
-                {'id': '3', 'last_name': 'user3'}]
+        return [{'id': '1', 'last_name': 'subject1'}, {'id': '2', 'last_name': 'subject2'},
+                {'id': '3', 'last_name': 'subject3'}]
 
-
-    def get_all_sessions_for_user(self, subject_id, from_date=None, to_date=None):
-        session = Session(self._read_test_session_json())
+    def get_all_sessions_for_subject(self, subject_id, from_date=None, to_date=None):
+        session = Session(self._read_test_session_json()['data'])
         # return list of dictionaries
         return [session]
 
-    def get_session_json(self, session_id, subject_id, embeds = SESSION_DATA_OUTPUTS):
+    def get_session_json(self, session_id):
         session = self._read_test_session_json()
-        session['session_id'] = session_id
-        session['user_id'] = subject_id;
+        # session['id'] = session_id
         return session
 
 
-class TestDataDownloader(unittest.TestCase):
+class TestDataDownloader():
 
-    def setUp(self):
-        self.mock_somnofy = MockSomnofy()
-        self.test_dir = tempfile.TemporaryDirectory()
-        self.mock_resolver = PathsResolver(path = self.test_dir.name)
-        self.data_downloader = DataDownloader(somnofy=self.mock_somnofy, resolver=self.mock_resolver)
+    mock_somnofy = MockSomnofy()
+    test_dir = tempfile.TemporaryDirectory()
+    mock_resolver = PathsResolver(path=test_dir.name)
+    data_downloader = DataDownloader(somnofy=mock_somnofy, resolver=mock_resolver)
 
     def test_init_without_somnofy_raises_error(self):
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             DataDownloader(somnofy=None, resolver=self.mock_resolver)
 
-    def test_save_user_data(self):
+    def test_save_subject_data(self):
+        subject = Subject({'id': '1', 'last_name': 'subject1', 'created_at': '2023-01-01T00:00:00'})
 
-        user = User({'id': '1', 'last_name': 'user1', 'created_at': '2023-01-01T00:00:00'})
-        subject_id = self.data_downloader.user_to_subject_id(user)
-
-        self.data_downloader.save_user_data(user, start_date = datetime.datetime.now() - datetime.timedelta(days=1))
+        self.data_downloader.save_subject_data(subject, start_date=datetime.datetime.now() - datetime.timedelta(days=1))
 
         s_json = self.mock_somnofy._read_test_session_json()
-        session_id = s_json["session_id"]
-        session = Session(s_json)
+        session_id = s_json['data']['id']
+        session = Session(s_json['data'])
         dates = self.data_downloader._sessions_to_date_range(session, session)
 
         # check if raw data was saved
-        raw_data_file = self.data_downloader._raw_session_file(s_json, subject_id, session_id)
-        self.assertTrue(os.path.isfile(raw_data_file))
+        raw_data_file = self.data_downloader._raw_session_file(s_json, subject.id, session_id)
+        assert os.path.isfile(raw_data_file)
 
         # check if epoch data was saved
-        epoch_data_file = self.data_downloader._epoch_data_file(subject_id, dates)
-        self.assertTrue(os.path.isfile(epoch_data_file))
+        epoch_data_file = self.data_downloader._epoch_data_file(subject.id, dates)
+        assert os.path.isfile(epoch_data_file)
 
         # check if reports were saved
-        reports_file = self.data_downloader._reports_file(subject_id, dates)
-        self.assertTrue(os.path.isfile(reports_file))
+        reports_file = self.data_downloader._reports_file(subject.id, dates)
+        assert os.path.isfile(reports_file)
 
         # check if last session was saved
-        last_session_file = self.mock_resolver.get_user_last_session(subject_id)
-        self.assertTrue(os.path.isfile(last_session_file))
+        last_session_file = self.mock_resolver.get_subject_last_session(subject.id)
+        assert os.path.isfile(last_session_file)
 
         # check if compliance data was saved
-        compliance_file = self.data_downloader._compliance_file(subject_id, dates)
-        self.assertTrue(os.path.isfile(compliance_file))
-
+        compliance_file = self.data_downloader._compliance_file(subject.id, dates)
+        assert os.path.isfile(compliance_file)
 
     def test_session_to_date_range(self):
         s_json = self.mock_somnofy._read_test_session_json()
-        s_json['start_time'] = '2024-07-20T22:00:00'
-        s_session = Session(s_json)
+        s_session = Session(s_json['data'])
         e_json = self.mock_somnofy._read_test_session_json()
-        e_json['end_time'] = '2024-07-21T06:00:00'
-        e_session = Session(e_json)
+        e_session = Session(e_json['data'])
 
         start_date, end_date = self.data_downloader._sessions_to_date_range(s_session, e_session)
-        self.assertEqual(start_date, datetime.date.fromisoformat('2024-07-20'))
-        self.assertEqual(end_date, datetime.date.fromisoformat('2024-07-21'))
-
-
+        assert start_date == datetime.date.fromisoformat('2024-12-25')
+        assert end_date == datetime.date.fromisoformat('2024-12-25')
 
     def test_calculate_start_date_with_proposed_date(self):
-        with self.assertRaises(ValueError):
-            self.data_downloader.calculate_start_date(subject_id='test_user')
+        with pytest.raises(ValueError):
+            self.data_downloader.calculate_start_date(subject_id='test_subject')
 
         proposed_date = '2020-01-01'
-        self.assertEqual(self.data_downloader.calculate_start_date(subject_id='test_user', proposed_date=proposed_date), proposed_date)
+        assert (self.data_downloader
+                .calculate_start_date(subject_id='test_subject', proposed_date=proposed_date)) == proposed_date
 
     def test_calculate_start_date_with_saved_date(self):
         proposed_date = '2020-01-01'
-        last_session_file = self.mock_resolver.get_user_last_session(subject_id='test_user')
-        last_session = {'start_time': '2023-01-01T00:00:00', 'end_time': '2023-01-01T02:00:00'}
+        last_session_file = self.mock_resolver.get_subject_last_session(subject_id='test_subject')
+        last_session = {'session_start': '2023-01-01T00:00:00', 'session_end': '2023-01-01T02:00:00'}
         try:
             with open(last_session_file, 'w') as f:
-                json.dump(last_session, f)
+                json.dump({'data': last_session}, f)
 
-            last_session['end_time'] = datetime.datetime.fromisoformat(last_session['end_time'])
-            self.assertEqual(self.data_downloader.calculate_start_date(subject_id='test_user', proposed_date=proposed_date, force_saved_date=True), last_session['end_time'])
+            last_session['session_end'] = datetime.datetime.fromisoformat(last_session['session_end'])
+            assert self.data_downloader.calculate_start_date(subject_id='test_subject',
+                                                             proposed_date=proposed_date,
+                                                             force_saved_date=True) == last_session['session_end']
         finally:
             os.remove(last_session_file)
 
     def test_calculate_start_date_with_session_duration_zero(self):
         proposed_date = '2020-01-01'
-        last_session_file = self.mock_resolver.get_user_last_session(subject_id='test_user')
-        last_session = {'start_time': '2023-01-01T00:00:00', 'end_time': '2023-01-01T00:00:00'}
+        last_session_file = self.mock_resolver.get_subject_last_session(subject_id='test_subject')
+        last_session = {'session_start': '2023-01-01T00:00:00', 'session_end': '2023-01-01T00:00:00'}
         try:
             with open(last_session_file, 'w') as f:
-                json.dump(last_session, f)
+                json.dump({'data': last_session}, f)
 
-            last_session['end_time'] = datetime.datetime.fromisoformat(last_session['end_time'])
-            start_date = self.data_downloader.calculate_start_date(subject_id='test_user', proposed_date=proposed_date, force_saved_date=True)
-            self.assertGreater(start_date, last_session['end_time'])
-            self.assertEqual(start_date.replace(microsecond=0), last_session['end_time'])
+            last_session['session_end'] = datetime.datetime.fromisoformat(last_session['session_end'])
+            start_date = self.data_downloader.calculate_start_date(subject_id='test_subject',
+                                                                   proposed_date=proposed_date,
+                                                                   force_saved_date=True)
+            assert start_date > last_session['session_end']
+            assert start_date.replace(microsecond=0) == last_session['session_end']
         finally:
             os.remove(last_session_file)
 
     def test_should_store_epoch_data(self):
-        # create session object
-        session = Session(self.mock_somnofy._read_test_session_json())
+        session = Session(self.mock_somnofy._read_test_session_json()['data'])
         session.duration_seconds = 3600
         self.data_downloader.ignore_epoch_for_shorter_than_hours = None
 
         result = self.data_downloader._should_store_epoch_data(session)
-        self.assertTrue(result)
+        assert result
 
         self.data_downloader.ignore_epoch_for_shorter_than_hours = 2
         result = self.data_downloader._should_store_epoch_data(session)
-        self.assertFalse(result)
+        assert not result
 
         self.data_downloader.ignore_epoch_for_shorter_than_hours = 1
         result = self.data_downloader._should_store_epoch_data(session)
-        self.assertTrue(result)
+        assert result
 
-
-
-    def tearDown(self):
+    def test_tearDown(self):
         self.test_dir.cleanup()
-
-if __name__ == '__main__':
-    unittest.main()
-
-
-
-
--- tests/test_paths_resolver.py
+++ tests/test_paths_resolver.py
@@ -1,57 +1,55 @@
-import unittest
 import tempfile
 import os
-from storage.paths_resolver import PathsResolver
+import pytest
+from ambient_bd_downloader.storage.paths_resolver import PathsResolver
 
-class TestPathsResolver(unittest.TestCase):
-    def setUp(self):
-        self.temp_dir = tempfile.TemporaryDirectory()
-        self.test_dir = self.temp_dir.name
-        self.resolver = PathsResolver(self.test_dir)
+
+class TestPathsResolver():
+
+    temp_dir = tempfile.TemporaryDirectory()
+    test_dir = temp_dir.name
+    resolver = PathsResolver(test_dir)
 
     def test_initialization(self):
-        self.assertEqual(self.resolver._main_dir, self.test_dir)
+        assert self.resolver._main_dir == self.test_dir
 
     def test_set_main_dir_creates_directory(self):
         test_main_dir = os.path.join(self.test_dir, 'main')
-        self.resolver.set_main_dir(test_main_dir)
-        self.assertTrue(os.path.exists(test_main_dir))
-        self.assertEqual(self.resolver.get_main_dir(), test_main_dir)
+        resolver = PathsResolver()
+        resolver.set_main_dir(test_main_dir)
+        assert os.path.exists(test_main_dir)
+        assert resolver.get_main_dir() == test_main_dir
 
     def test_set_main_dir_raises_exception(self):
         path = os.path.join(self.test_dir, 'file.txt')
         with open(path, 'w') as temp_file:
             temp_file.write('This is a temporary file.')
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             self.resolver.set_main_dir(path)
 
-    def test_get_user_dir(self):
-        #self.resolver.set_main_dir(self.test_dir)
-        expected_path = os.path.join(self.test_dir, 'user1')
-        self.assertEqual(self.resolver.get_user_dir('user1'), expected_path)
+    def test_get_subject_dir(self):
+        expected_path = os.path.join(self.test_dir, 'subject1')
+        assert self.resolver.get_subject_dir('subject1') == expected_path
 
-    def test_get_user_sys_dir(self):
-        #self.resolver.set_main_dir(self.test_dir)
-        expected_path = os.path.join(self.test_dir, 'user1', 'sys')
-        self.assertEqual(self.resolver.get_user_sys_dir('user1'), expected_path)
+    def test_get_subject_sys_dir(self):
+        expected_path = os.path.join(self.test_dir, 'subject1', 'sys')
+        assert self.resolver.get_subject_sys_dir('subject1') == expected_path
 
-    def test_get_user_data_dir(self):
-        #self.resolver.set_main_dir(self.test_dir)
-        expected_path = os.path.join(self.test_dir, 'user1', 'data')
-        self.assertEqual(self.resolver.get_user_data_dir('user1'), expected_path)
+    def test_get_subject_data_dir(self):
+        expected_path = os.path.join(self.test_dir, 'subject1', 'data')
+        assert self.resolver.get_subject_data_dir('subject1') == expected_path
 
-    def test_get_user_raw_dir(self):
-        #self.resolver.set_main_dir(self.test_dir)
-        expected_path = os.path.join(self.test_dir, 'user1', 'raw')
-        self.assertEqual(self.resolver.get_user_raw_dir('user1'), expected_path)
+    def test_get_subject_raw_dir(self):
+        expected_path = os.path.join(self.test_dir, 'subject1', 'raw')
+        assert self.resolver.get_subject_raw_dir('subject1') == expected_path
 
-    def test_get_user_last_session(self):
-        #self.resolver.set_main_dir(self.test_dir)
-        expected_path = os.path.join(self.test_dir, 'user1', 'sys', 'last_session.json')
-        self.assertEqual(self.resolver.get_user_last_session('user1'), expected_path)
+    def test_get_subject_last_session(self):
+        expected_path = os.path.join(self.test_dir, 'subject1', 'sys', 'last_session.json')
+        assert self.resolver.get_subject_last_session('subject1') == expected_path
 
-    def tearDown(self):
+    def test_get_subject_global_report(self):
+        expected_path = os.path.join(self.test_dir, 'subject1', 'data', 'all_sessions_report.csv')
+        assert self.resolver.get_subject_global_report('subject1') == expected_path
+
+    def test_tearDown(self):
         self.temp_dir.cleanup()
-
-if __name__ == '__main__':
-    unittest.main()
